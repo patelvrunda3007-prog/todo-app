4b58e9e6bcf1944b473c8e1a4899fab03fa255a6
patelvrunda3007-prog <patelvrunda3007@gmail.com>
2026-06-05 09:47:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function save(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+function addTask(){
+
+    let input = document.getElementById("taskInput");
+
+    if(input.value.trim()=="") return;
+
+    tasks.push({
+        text:input.value,
+        completed:false
+    });
+
+    input.value="";
+    save();
+    render();
+}
+
+function render(){
+
+    let list = document.getElementById("taskList");
+    list.innerHTML="";
+
+    let completed = 0;
+
+    tasks.forEach((t,i)=>{
+
+        if(t.completed) completed++;
+
+        let li = document.createElement("li");
+
+        li.innerHTML=`
+            <span class="${t.completed?'completed':''}">
+                ${t.text}
+            </span>
+
+            <div>
+                <button onclick="done(${i})">✔</button>
+                <button onclick="del(${i})">❌</button>
+            </div>
+        `;
+
+        list.appendChild(li);
+    });
+
+    document.getElementById("count").innerText =
+        `Tasks: ${tasks.length}`;
+
+    let percent = tasks.length? (completed/tasks.length)*100 : 0;
+    document.getElementById("progress").style.width = percent + "%";
+}
+
+function done(i){
+
+    tasks[i].completed = !tasks[i].completed;
+
+    confetti({
+        particleCount: 80,
+        spread: 70
+    });
+
+    save();
+    render();
+}
+
+function del(i){
+    tasks.splice(i,1);
+    save();
+    render();
+}
+
+function toggleMode(){
+    document.body.classList.toggle("dark");
+}
+
+render();
